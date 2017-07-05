66e8a5893399db9531e22f6e62691b75e9d47a51
Janos Ujvari <janos.ujvari@pangea.ch>
2017-07-05 15:36:11 +0200

    assignment 8

---
+++ Assignment8/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="setSelectedComponent('appBlue')">Load Blue Template</button>
+                <button class="btn btn-success" @click="setSelectedComponent('appGreen')">Load Green Template</button>
+                <button class="btn btn-danger"  @click="setSelectedComponent('app-red')">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <!-- None selected! -->
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data(){
+            return {
+                selectedComponent: 'appBlue'
+            }
+        },
+        methods:{
+            setSelectedComponent(comp){
+                this.selectedComponent = comp;
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Assignment8/src/components/Red.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    	<!-- default slot -->
+    	<slot></slot>
+    	<hr>
+    	<slot name="named"></slot>
+    	<hr width="80%">
+    	<slot name="notdeclared">Not declared one. Red</slot>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+        background-color: lightcoral;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
